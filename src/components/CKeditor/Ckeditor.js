79db5700d07e4de43e41cd1856017a7059d4858a
ankitpatil <ankitp@elicit.digital>
2024-06-28 16:36:12 +0530

    CKeditorPatchData

--- src/components/CKeditor/Ckeditor.js
+++ src/components/CKeditor/Ckeditor.js
@@ -3,11 +3,13 @@ import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import axios from 'axios';
 import { Button } from 'antd';
+const { REACT_APP_DETAILS_URL } = process.env;
 
 const Ckeditor = ({ id }) => {
   const [stepDescription, setStepDescription] = useState('');
   const [editorData, setEditorData] = useState('');
-
+  const token = localStorage.getItem('token');
+  console.log(token, 'token');
   class MyUploadAdapter {
     constructor(loader) {
       this.loader = loader;
@@ -21,14 +23,15 @@ const Ckeditor = ({ id }) => {
             formData.append('image', file);
 
             axios
-              .post('http://192.168.29.229:3004/api/process/add-image', formData, {
+              .post(`${REACT_APP_DETAILS_URL}process/add-image`, formData, {
                 headers: {
                   'Content-Type': 'multipart/form-data'
                 }
               })
               .then((response) => {
-                const imageUrl = response.data.url; // Assuming the response contains the image URL in `response.data.url`
-                console.log(imageUrl);
+                const imageUrl = response?.data?.result?.url;
+                console.log('URL', response?.data?.result?.url);
+                console.log('response', response);
                 resolve({
                   default: imageUrl
                 });
@@ -64,9 +67,10 @@ const Ckeditor = ({ id }) => {
     console.log('Submitting form with payload:', payload);
 
     axios
-      .patch('http://192.168.29.229:3004/api/process', payload, {
+      .patch(`${REACT_APP_DETAILS_URL}process`, payload, {
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
         }
       })
       .then((res) => {
@@ -105,242 +109,3 @@ const Ckeditor = ({ id }) => {
 };
 
 export default Ckeditor;
-
-// import React, { useState } from 'react';
-// import { CKEditor } from '@ckeditor/ckeditor5-react';
-// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-// import axios from 'axios';
-// import { Button } from 'antd';
-
-// const Ckeditor = ({ id }) => {
-//   const [stepDescription, setStepDescription] = useState('');
-//   const [editorData, setEditorData] = useState('');
-
-//   class MyUploadAdapter {
-//     constructor(loader) {
-//       this.loader = loader;
-//     }
-
-//     upload() {
-//       return this.loader.file.then(
-//         (file) =>
-//           new Promise((resolve, reject) => {
-//             const formData = new FormData();
-//             formData.append('image', file);
-
-//             axios
-//               .post('http://192.168.29.229:3004/api/process/add-image', formData, {
-//                 headers: {
-//                   'Content-Type': 'multipart/form-data'
-//                 }
-//               })
-//               .then((response) => {
-//                 const imageUrl = response.data.url; // Assuming the response contains the image URL in `response.data.url`
-//                 console.log('<img src="' + imageUrl + '"/>');
-//                 resolve({
-//                   default: imageUrl
-//                 });
-//               })
-//               .catch((error) => {
-//                 reject(error);
-//               });
-//           })
-//       );
-//     }
-
-//     abort() {
-//       // Handle abort
-//     }
-//   }
-
-//   function MyCustomUploadAdapterPlugin(editor) {
-//     editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
-//       return new MyUploadAdapter(loader);
-//     };
-//   }
-
-//   const editorConfiguration = {
-//     extraPlugins: [MyCustomUploadAdapterPlugin],
-//     toolbar: {
-//       items: [
-//         'heading',
-//         '|',
-//         'bold',
-//         'italic',
-//         'link',
-//         'bulletedList',
-//         'numberedList',
-//         'blockQuote',
-//         'insertTable',
-//         'undo',
-//         'redo',
-//         '|',
-//         'imageUpload',
-//         'mediaEmbed',
-//         'removeFormat'
-//       ]
-//     },
-//     image: {
-//       toolbar: ['imageTextAlternative', 'imageStyle:full', 'imageStyle:side']
-//     }
-//   };
-
-//   const submitForm = () => {
-//     const payload = {
-//       id,
-//       stepDescription
-//     };
-
-//     console.log('Submitting form with payload:', payload);
-
-//     axios
-//       .patch('http://192.168.29.229:3004/api/process', payload, {
-//         headers: {
-//           'Content-Type': 'application/json'
-//         }
-//       })
-//       .then((res) => {
-//         console.log('Response from API:', res);
-//       })
-//       .catch((error) => {
-//         console.error('Error while submitting form:', error);
-//       });
-//   };
-
-//   return (
-//     <div>
-//       <Button onClick={submitForm}>Add</Button>
-//       <CKEditor
-//         editor={ClassicEditor}
-//         config={editorConfiguration}
-//         data={editorData}
-//         onReady={(editor) => {
-//           console.log('Editor is ready to use!', editor);
-//         }}
-//         onChange={(event, editor) => {
-//           const data = editor.getData();
-//           console.log('Editor data:', data);
-//           setEditorData(data);
-//           setStepDescription(data);
-//         }}
-//         onBlur={(event, editor) => {
-//           console.log('Editor blurred.', editor);
-//         }}
-//         onFocus={(event, editor) => {
-//           console.log('Editor focused.', editor);
-//         }}
-//       />
-//     </div>
-//   );
-// };
-
-// export default Ckeditor;
-
-// import React, { useState } from 'react';
-// import { CKEditor } from '@ckeditor/ckeditor5-react';
-// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-// import axios from 'axios';
-// import { Button } from 'antd';
-
-// const Ckeditor = ({ id }) => {
-//   const [stepDescription, setStepDescription] = useState('');
-//   const [editorData, setEditorData] = useState('');
-
-//   class MyUploadAdapter {
-//     constructor(loader) {
-//       this.loader = loader;
-//     }
-
-//     upload() {
-//       return this.loader.file.then(
-//         (file) =>
-//           new Promise((resolve, reject) => {
-//             const formData = new FormData();
-//             formData.append('image', file);
-
-//             axios
-//               .post('http://192.168.29.229:3004/api/process/add-image', formData, {
-//                 headers: {
-//                   'Content-Type': 'multipart/form-data'
-//                 }
-//               })
-//               .then((response) => {
-//                 const imageUrl = response?.data?.url; // Assuming the response contains the image URL in `response.data.url`
-//                 console.log('<img src="' + imageUrl + '"/>');
-//                 resolve({
-//                   default: imageUrl
-//                 });
-//               })
-//               .catch((error) => {
-//                 reject(error);
-//               });
-//           })
-//       );
-//     }
-
-//     abort() {
-//       // Handle abort
-//     }
-//   }
-
-//   function MyCustomUploadAdapterPlugin(editor) {
-//     editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
-//       return new MyUploadAdapter(loader);
-//     };
-//   }
-
-//   const editorConfiguration = {
-//     extraPlugins: [MyCustomUploadAdapterPlugin]
-
-//   };
-
-//   const submitForm = () => {
-//     const payload = {
-//       id,
-//       stepDescription
-//     };
-
-//     console.log('Submitting form with payload:', payload);
-
-//     axios
-//       .patch('http://your-api-url/process', payload, {
-//         headers: {
-//           'Content-Type': 'application/json'
-//         }
-//       })
-//       .then((res) => {
-//         console.log('Response from API:', res);
-//       })
-//       .catch((error) => {
-//         console.error('Error while submitting form:', error);
-//       });
-//   };
-
-//   return (
-//     <div>
-//       <Button onClick={submitForm}>Add</Button>
-//       <CKEditor
-//         editor={ClassicEditor}
-//         config={editorConfiguration}
-//         data={editorData}
-//         onReady={(editor) => {
-//           console.log('Editor is ready to use!', editor);
-//         }}
-//         onChange={(event, editor) => {
-//           const data = editor.getData();
-//           console.log('Editor data:', data);
-//           setEditorData(data);
-//           setStepDescription(data);
-//         }}
-//         onBlur={(event, editor) => {
-//           console.log('Editor blurred.', editor);
-//         }}
-//         onFocus={(event, editor) => {
-//           console.log('Editor focused.', editor);
-//         }}
-//       />
-//     </div>
-//   );
-// };
-
-// export default Ckeditor;
--- src/layout/HomeLayout/HomeLayout.js
+++ src/layout/HomeLayout/HomeLayout.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { ButtonContainer, Header, LogoContainer, MainContainer } from "./Style";
-import { Link, Outlet } from "react-router-dom";
-import logo from "../../assets/images/logo.34ac6a4edb0bef53937e.jpg";
+import React from 'react';
+import { ButtonContainer, Header, LogoContainer, MainContainer } from './Style';
+import { Link, Outlet } from 'react-router-dom';
+import logo from '../../assets/images/logo.34ac6a4edb0bef53937e.jpg';
 
 const HomeLayout = () => {
   return (
